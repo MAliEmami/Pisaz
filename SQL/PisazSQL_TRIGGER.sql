c557f78e6ca871db988110e023cfd31a6a67aee2
MAEmamizade <aliem2004.3@gmail.com>
2025-02-09 16:13:48 +0330

    add ToBeVIP, BuyProduct, BuyVIPAccount, ChargeWallet  trigger

--- SQL/PisazSQL_TRIGGER.sql
+++ SQL/PisazSQL_TRIGGER.sql
@@ -9,13 +9,13 @@ BEGIN
 END;
 
 
-CREATE TRIGGER VIPDepo
+CREATE TRIGGER VIPDepo --i think its dont work
 ON IssuedFor
 AFTER INSERT
 AS
 BEGIN
     UPDATE Client
-    SET C.WalletBalance = C.WalletBalance + (SUM(CurrentPrice) * 0.15)
+    SET C.WalletBalance := C.WalletBalance + (SUM(CurrentPrice) * 0.15)
     FROM INSERTED I, LockedShoppingCart L, Client C, AddedTo A, Product P, VIPClients V
     WHERE
         C.Id = V.Id
@@ -34,4 +34,49 @@ BEGIN
     GROUP BY L.LockedNumber
 END;
 
--- CREATE TRIGGER ChargeWallet
+CREATE TRIGGER ChargeWallet
+ON DepositsIntoWallet 
+AFTER INSERT
+AS
+BEGIN
+    UPDATE Client
+    SET C.WalletBalance := C.WalletBalance + I.Amount
+    FROM INSERTED I JOIN Client C ON I.Id = C.Id
+END;
+
+CREATE TRIGGER BuyVIPAccount
+ON Subscribes
+BEFORE INSERT
+AS
+BEGIN
+    UPDATE Client
+    SET C.WalletBalance := C.WalletBalance - 300000
+    FROM (INSERTED I JOIN DepositsIntoWallet D) JOIN Client C
+END;
+
+CREATE TRIGGER BuyProduct
+ON IssuedFor
+BEFORE INSERT
+AS
+BEGIN
+END;
+
+
+CREATE TRIGGER ToBeVIP
+ON Subscribes
+AFTER INSERT
+AS
+BEGIN
+    INSERT INTO VIPClients(Id, SubsctiptionExpirationTime)
+    SELECT C.Id, DATEADD(DAY, 30, GETDATE())
+    FROM INSERTED I JOIN Client C ON I.Id = C.Id
+END;
+
+-- CREATE TRIGGER RemoveExpiredVIP -its dont work
+-- ON Vip
+-- AFTER INSERT, UPDATE -- its should be change to period time
+-- AS
+-- BEGIN
+--     DELETE FROM VIPClients
+--     WHERE DATEDIFF(DAY, SubscriptionExpirationTime, GETDATE()) > 30;
+-- END;
