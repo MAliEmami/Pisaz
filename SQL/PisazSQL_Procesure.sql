d7a7548ad378eb93715472a0edccd533f73ea5fa
Nimatrixx <www.nimatrix@gmail.com>
2025-02-14 23:03:38 +0330

    satisfy constraint number 12 by a Job And a TRIGGER

--- SQL/PisazSQL_Procesure.sql
+++ SQL/PisazSQL_Procesure.sql
@@ -89,14 +89,55 @@ EXEC sp_add_jobstep
 GO
 -- creates a schedule named DailyJobs
 EXEC sp_add_schedule
-    @schedule_name = N'DailyJobs' ,
+    @schedule_name = N'DailyJobs2' ,
     @freq_type = 4, -- 4 : Daily repeat
-    @freq_interval = 1, -- 1 : Day 1 of each month
+    @freq_interval = 1, -- 1 : Everyday
 	@freq_recurrence_factor = 1,
     @active_start_time = 023400 ; -- Start at 02:34 AM
 GO
 -- attaches the schedule to the job
 EXEC sp_attach_schedule
     @job_name = N'VIPValidation',
-    @schedule_name = N'DailyJobs' ;
+    @schedule_name = N'DailyJobs2' ; 
+GO
+
+
+-- creates a job to check locked cart expiration time
+GO
+EXEC dbo.sp_add_job @job_name = N'LockedCartChecker' ;
+
+GO
+EXEC sp_add_jobstep
+    @job_name = N'LockedCartChecker',
+	@step_id = 1,
+    @step_name = N'Check',
+    @subsystem = N'TSQL',
+    @command = 
+	N'
+	UPDATE ShoppingCart
+	SET CartStatus = ''blocked''
+	FROM (SELECT * FROM ShoppingCart WHERE CartStatus = ''Locked'') AS S JOIN LockedShoppingCart AS L ON S.ID = L.ID AND S.CartNumber = L.CartNumber
+	WHERE  DATEADD(DAY, 3,LockTimestamp) < GETDATE(); 
+
+	UPDATE ShoppingCart
+	SET CartStatus = ''active''
+	FROM (SELECT * FROM ShoppingCart WHERE CartStatus = ''Locked'') AS S JOIN LockedShoppingCart AS L ON S.ID = L.ID AND S.CartNumber = L.CartNumber
+	WHERE  DATEADD(DAY, 7,LockTimestamp) < GETDATE(); 
+	',
+	@database_name = N'Pisaz',
+    @retry_attempts = 5,
+    @retry_interval = 5 ;
+GO
+-- creates a schedule named DailyJobs3
+EXEC sp_add_schedule
+    @schedule_name = N'DailyJobs3' ,
+    @freq_type = 4, -- 4 : Daily repeat
+    @freq_interval = 1, -- 1 : Everyday
+	@freq_recurrence_factor = 1,
+    @active_start_time = 033000 ; -- Start at 03:30 AM
+GO
+-- attaches the schedule to the job
+EXEC sp_attach_schedule
+    @job_name = N'LockedCartChecker',
+    @schedule_name = N'DailyJobs3' ; 
 GO
